f3dfa510bf0e85c6cc4c5e5f2e784d594779e1ec
yezh0 <yezh0913@gmail.com>
2019-03-29 12:13:36 -0400

    ui update && database model

--- Programming Files/networkoptimizer/optimizer/models.py
+++ Programming Files/networkoptimizer/optimizer/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Search(models.Model):
+    capacity = models.CharField(max_length=200)
+    quality = models.CharField(max_length=200)
+
+
+class Provider(models.Model):
+    provider_id = models.CharField(max_length=200)
+    provider_name = models.CharField(max_length=200)
+    cost = models.CharField(max_length=200)
+    quality = models.CharField(max_length=200)
+    search = models.ForeignKey(Search, related_name="search", on_delete=models.CASCADE)
+
+
+class CustomList(models.Model):
+    must_exist = models.BooleanField()
+    must_remove = models.BooleanField()
+
+
+
+
+
+
