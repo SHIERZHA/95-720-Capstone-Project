3623e3dca529c41a938e1a9ea62c9a04bbde40c0
cyrann <553614753@qq.com>
2019-04-18 16:13:35 -0400

    Add html template for second_model.

--- Programming Files/networkoptimizer/optimizer/urls.py
+++ Programming Files/networkoptimizer/optimizer/urls.py
@@ -5,5 +5,6 @@ from optimizer import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('optimize', views.optimize, name='optimize'),
+    path('re_optimize', views.re_optimize, name='re_optimize'),
     path('reset', views.reset, name='reset')
 ]
--- Programming Files/networkoptimizer/optimizer/views.py
+++ Programming Files/networkoptimizer/optimizer/views.py
@@ -5,6 +5,7 @@ import numpy as np
 # Create your views here.
 
 # Cache the processed data
+county_name = ''
 first_df = None
 # second_df = None
 provider_info = None
@@ -27,6 +28,18 @@ min_rating_init = 0
 
 def home(request):
     global provider_info
+    global feature_df
+    global enrollment_info
+    feature_df = None
+    # read file - takes a few seconds. Patience is appreciated
+    if provider_info is None:
+        provider_info = pd.read_csv('optimizer/static/dataset/provider_data.csv')
+        enrollment_info = pd.read_csv('optimizer/static/dataset/enrollment_by_county.csv')
+    return render(request, 'index.html', context={"first_result": first_result, "second_result": second_result})
+
+
+def reset(request):
+    global provider_info
     global enrollment_info
     global feature_df
     global county_provider_data
@@ -47,25 +60,15 @@ def home(request):
     first_cost = 0
     first_avg_score = 0
     second_cost = 0
-
-    feature_df = None
-    # read file - takes a few seconds. Patience is appreciated
-    if provider_info is None:
-        provider_info = pd.read_csv('optimizer/static/dataset/provider_data.csv')
-        enrollment_info = pd.read_csv('optimizer/static/dataset/enrollment_by_county.csv')
-    return render(request, 'index.html', context={"first_result": first_result, "second_result": second_result})
-
-
-def reset(request):
-
     return home(request)
 
 
-def optimize(request):
+def re_optimize(request):
     global cm_init
     global ui_init
     global turnover_init
     global min_rating_init
+    global county_name
     county = request.GET['county']
     cm = float(request.GET['cm'])
     ui = float(request.GET['ui'])
@@ -87,15 +90,12 @@ def optimize(request):
     global first_avg_score
     global second_cost
     global county_provider_data
-    second_df = None
-    second_avg_score = 0
-    first = False
-    if feature_df is None:
+
+    if county != county_name:
         cm_init = cm
         ui_init = ui
         turnover_init = turnover
         min_rating_init = min_rating
-        first = True
         # build the feature matrix
         filename = county + '.csv'
         distance_info = pd.read_csv('optimizer/static/distance/' + filename, encoding='ISO-8859-1')
@@ -108,8 +108,6 @@ def optimize(request):
 
         county_provider_data = county_provider_data.reset_index(drop=True)
         county_provider_data = county_provider_data.rename(columns={'Total SNF Medicare Payment Amount': 'COST'})
-        # debug
-        # print(county_provider_data)
 
         distance_matrix = []
         for i in range(len(county_provider_data)):
@@ -153,6 +151,30 @@ def optimize(request):
     model_df, costs, hold_capacity = change_providers(feature_df, county_provider_data, to_delete, to_add)
     # Overall rating constraint
     model_df = add_constraint(model_df, min_rating, 'OVERALL_RATING', True)
+    model_df, selected_providers, total_cost = execute_model(cm, county, county_provider_data, hold_capacity,
+                                                             model_df, to_add, turnover, ui)
+    second_df = selected_providers
+    second_avg_score = second_df['OVERALL_RATING'].mean()
+    if total_cost.value is None:
+        second_result = False
+    else:
+        second_cost = total_cost.value + costs
+    print("first score", first_avg_score)
+    print("second score", second_avg_score)
+    print("first cost", first_cost)
+    print("second cost", second_cost)
+    print("first result", first_result)
+    print("second result", second_result)
+    county_name = county
+    return render(request, 'second_model.html',
+                  context={"first_df": first_df, "second_df": second_df, "first_avg_score": first_avg_score,
+                           "second_avg_score": second_avg_score, "first_cost": first_cost, "second_cost": second_cost,
+                           "first_result": first_result, "second_result": second_result, 'county': county, 'cm': cm_init,
+                           'ui': ui_init, 'turnover': turnover_init, 'min_rating': min_rating_init, 'to_add': to_add_orig,
+                           'to_delete': to_delete_orig})
+
+
+def execute_model(cm, county, county_provider_data, hold_capacity, model_df, to_add, turnover, ui):
     # Build constraint matrix
     # min_rating = min_rating
     A = []
@@ -164,68 +186,148 @@ def optimize(request):
         tmp = model_df.iloc[i, 6:].values.astype('float64')
         tmp[i] = -1
         A.append(tmp)
-
     #  Build the upper bound (right side) of the inequality
     B = []
     # Bed number upper bound
     added_index = county_provider_data[county_provider_data['PROVNUM'].isin(to_add)].index
     added_provider_beds = county_provider_data['BEDCERT'][added_index].sum()
-    enrollment = enrollment_info[enrollment_info['COUNTY_NAME'] == county].reset_index()['ENROLLMENT'][0] - added_provider_beds
+    enrollment = enrollment_info[enrollment_info['COUNTY_NAME'] == county].reset_index()['ENROLLMENT'][
+                     0] - added_provider_beds
     B.append((enrollment * cm * ui * turnover / 365) - hold_capacity)
-
     # Neighbor upper bound
     for i in range(sample_size):
         B.append(0)
-
     # Build the object function
     objective_func = model_df['COST'].values.T
-
     import cvxpy
     selection = cvxpy.Variable((sample_size), boolean=True, integer=True)
-
     total_cost = np.array(objective_func) * selection
-
     # constraints = np.array([a * selection <= B for a in np.array(A)])
     B = np.array(B).reshape(-1, 1)
     constraints = [A[i] * selection >= B[i] for i in range(len(model_df) + 1)]
     # constraints = feature_df['BEDCERT'].values.T * selection >= 5000
-
     network_optimization = cvxpy.Problem(cvxpy.Minimize(total_cost), constraints)
-
     network_optimization.solve(solver=cvxpy.GLPK_MI)
-
     # print(total_cost.value + costs)
     print('enrollment', enrollment)
     print('capacity constraints', enrollment * cm * ui * turnover / 365)
-
     selected_providers_index = np.concatenate((model_df[selection.value == 1].index, added_index), axis=None)
     selected_providers = county_provider_data.iloc[selected_providers_index, :]
+    return model_df, selected_providers, total_cost
+
+
+def optimize(request):
+    global cm_init
+    global ui_init
+    global turnover_init
+    global min_rating_init
+    global county_name
+    county = request.GET['county']
+    cm = float(request.GET['cm'])
+    ui = float(request.GET['ui'])
+    turnover = float(request.GET['turnover'])
+    min_rating = int(request.GET['min_rating'])
+    to_delete = request.GET['to_delete']
+    to_add = request.GET['to_add']
+    to_delete_orig = to_delete
+    to_add_orig = to_add
+    to_delete = to_delete.replace(' ', '').split(';')
+    to_add = to_add.replace(' ', '').split(';')
+
+    global feature_df
+    global first_df
+    global provider_info
+    global first_result
+    global second_result
+    global first_cost
+    global first_avg_score
+    global second_cost
+    global county_provider_data
+
+    cm_init = cm
+    ui_init = ui
+    turnover_init = turnover
+    min_rating_init = min_rating
+    # build the feature matrix
+    filename = county + '.csv'
+    distance_info = pd.read_csv('optimizer/static/distance/' + filename, encoding='ISO-8859-1')
+
+    distance_info['origin_id'] = distance_info['origin_id'].astype('str')
+    distance_info['destination_id'] = distance_info['destination_id'].astype('str')
+
+    county_provider_data = provider_info[
+        (provider_info['STATE'] == 'PA') & (provider_info['COUNTY_NAME'] == county)]
+
+    county_provider_data = county_provider_data.reset_index(drop=True)
+    county_provider_data = county_provider_data.rename(columns={'Total SNF Medicare Payment Amount': 'COST'})
+    # debug
+    # print(county_provider_data)
+
+    distance_matrix = []
+    for i in range(len(county_provider_data)):
+        distance_arr = []
+        original_id = str(county_provider_data.iloc[i, 0])
+        # print("original", original_id)
+        for j in range(len(county_provider_data)):
+            if i == j:
+                distance_arr.append(0.0)
+                continue
+            destination_id = str(county_provider_data.iloc[j, 0])
+            distance_df = distance_info[((distance_info['origin_id'] == original_id) &
+                                         (distance_info['destination_id'] == destination_id)) |
+                                        ((distance_info['origin_id'] == destination_id) &
+                                         (distance_info['destination_id'] == original_id))]
+            # print(distance_df)
+            if distance_df.reset_index()['DISTANCE'][0] > 20:
+                distance_arr.append(0.0)
+            else:
+                distance_arr.append(1.0)
+        print(distance_arr)
+        distance_arr.append(original_id)
+        distance_matrix.append(distance_arr)
+
+    distance_matrix = np.array(distance_matrix)
+    # distance_matrix.shape
+    distance_df = pd.DataFrame(distance_matrix)
+    distance_df.rename(columns={distance_df.columns[len(distance_df)]: "PROVNUM"}, inplace=True)
+    county_provider_data['PROVNUM'] = county_provider_data['PROVNUM'].astype('str')
+    all_feature_df = pd.merge(county_provider_data, distance_df, on=['PROVNUM'])
+
+    distance_index = list(range(15, 15 + len(county_provider_data)))
+    index = [2, 5, 9, 12, 13, 14]
+    index = index + distance_index
+    # print(index)
+    # all_feature_df[list(range(1,60))] = all_feature_df[list(range(1,60))].astype('float')
+    feature_df = all_feature_df.iloc[:, index]
+
+    # all_feature_df[list(range(1, 60))] = all_feature_df[list(range(1, 60))].astype('float')
+
+    model_df, costs, hold_capacity = change_providers(feature_df, county_provider_data, to_delete, to_add)
+    # Overall rating constraint
+    model_df = add_constraint(model_df, min_rating, 'OVERALL_RATING', True)
+    model_df, selected_providers, total_cost = execute_model(cm, county, county_provider_data, hold_capacity,
+                                                             model_df, to_add, turnover, ui)
+
     # cache to global variable
-    if first:
-        first_df = selected_providers
-        first_avg_score = first_df['OVERALL_RATING'].mean()
-        if total_cost.value is None:
-            first_result = False
-            feature_df = None
-        else:
-            first_cost = total_cost.value + costs
-            feature_df = model_df
+
+    first_df = selected_providers
+    first_avg_score = first_df['OVERALL_RATING'].mean()
+    if total_cost.value is None:
+        first_result = False
+        feature_df = None
     else:
-        second_df = selected_providers
-        second_avg_score = second_df['OVERALL_RATING'].mean()
-        if total_cost.value is None:
-            second_result = False
-        else:
-            second_cost = total_cost.value + costs
+        first_cost = total_cost.value + costs
+        feature_df = model_df
     print("first score", first_avg_score)
-    print("second score", second_avg_score)
+    print("second score", 0)
     print("first cost", first_cost)
     print("second cost", second_cost)
     print("first result", first_result)
     print("second result", second_result)
-    return render(request, 'index.html',
-                  context={"first_df": first_df, "second_df": second_df, "first_avg_score": first_avg_score,
-                           "second_avg_score": second_avg_score, "first_cost": first_cost, "second_cost": second_cost,
+    county_name = county
+    return render(request, 'second_model.html',
+                  context={"first_df": first_df, "second_df": None, "first_avg_score": first_avg_score,
+                           "second_avg_score": 0, "first_cost": first_cost, "second_cost": second_cost,
                            "first_result": first_result, "second_result": second_result, 'county': county, 'cm': cm_init,
                            'ui': ui_init, 'turnover': turnover_init, 'min_rating': min_rating_init, 'to_add': to_add_orig,
                            'to_delete': to_delete_orig})
